cc664f9be1210f53cfd8f491e96c0a8f428c677a
IsKJH <dkdlrh147@gmail.com>
2025-07-02 20:01:04 +0900

    [init] 초기 백엔드 구축 - 김지한

---
+++ spb/src/test/java/sp/spb/SpbApplicationTests.java
@@ -0,0 +1,13 @@
+package sp.spb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
